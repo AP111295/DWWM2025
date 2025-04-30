591f9d54b4d0c039c909aa67274cd4e52af9a1d3
Pandey <archana.pandey111295@gmail.com>
2025-04-30 13:56:51 +0200

    doc: adding new changes

--- 04-javascript/00-bonus/shop/script.js
+++ 04-javascript/00-bonus/shop/script.js
@@ -1,17 +1,37 @@
 "use strict";
 
-fetch('https://fakestoreapi.com/products')
-  .then(response => response.json())
-  .then(data => console.log(data));
+    const productsDiv = document.getElementById("products");
+    const cartCount = document.getElementById("cart-count");
+    const cartTotal = document.getElementById("cart-total");
 
+    let cart = [];
 
-  [
-    {
-      "id": 0,
-      "title": "string",
-      "price": 0.1,
-      "description": "string",
-      "category": "string",
-      "image": "http://example.com"
+    function updateCart() {
+      cartCount.textContent = cart.length;
+      const total = cart.reduce((sum, item) => sum + item.price, 0);
+      cartTotal.textContent = total.toFixed(2);
     }
-  ]
+ function addToCart(product)
+ {
+  cart.push(product);
+  updateCart();
+ }
+ fetch("https://fakestoreapi.com/products")
+ .then(Response => Response.json())
+ .then(product => {
+  product.forEach(product => {
+     const div = document.createElement("div");
+     div.className = "product";    
+     div.innerHTML = `
+     <h3>${product.title}</h3>
+     <img src= "${product.image}" >
+     <p> price: $${product.price}</p>
+     <button onclick= 'addTocart(${JSON.stringify(product)})'>Add to cart</button>
+     `;
+     productsDiv.appendChild(div);
+  });
+ })
+
+
+
+
